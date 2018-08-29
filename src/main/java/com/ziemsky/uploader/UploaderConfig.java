2fa04ef9bf163261bb5ed6f1a5b7100e4bb4ee6c
Rafał Kociński <rk@ziemsky.com>
2018-08-29 21:39:15 +0100

    WIP

--- src/main/java/com/ziemsky/uploader/UploaderConfig.java
+++ src/main/java/com/ziemsky/uploader/UploaderConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.dsl.AggregatorSpec;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.channel.MessageChannels;
@@ -26,7 +27,10 @@ import static org.springframework.integration.file.dsl.Files.inboundAdapter;
 @EnableIntegration
 public class UploaderConfig {
 
-    private static final int MAX_MESSAGES_PER_POLL = 10;
+    private static final int BATCH_SIZE = 10;
+
+    public static final String FILES_QUEUE_CHANNEL = "filesQueueChannel";
+    public static final String BATCH_QUEUE_CHANNEL = "batchQueueChannel";
 
     final Logger log = LoggerFactory.getLogger(UploaderConfig.class);
 
@@ -41,43 +45,44 @@ public class UploaderConfig {
 
         log.info("Monitoring {}", inboundDir);
 
-        return IntegrationFlows.from(inboundAdapter(
-            // todo switch from polling to watch service? See docs for caveats!:
-            // https://docs.spring.io/spring-integration/reference/html/files.html#watch-service-directory-scanner
-            inboundDir.toFile(),
-            Comparator.comparing(
-                File::getName,
-                (leftFileName, rightFileName) -> -1 * leftFileName.compareTo(rightFileName)
-            ))
-            .patternFilter("*.jpg")
+        return IntegrationFlows.from(
+            inboundAdapter(
+                // todo switch from polling to watch service? See docs for caveats!:
+                // https://docs.spring.io/spring-integration/reference/html/files.html#watch-service-directory-scanner
+                inboundDir.toFile(),
+                Comparator.comparing(
+                    File::getName,
+                    (leftFileName, rightFileName) -> -1 * leftFileName.compareTo(rightFileName)
+                ))
+                .patternFilter("*.jpg")
         )
-            .channel("filesQueueChannel")
+            .channel(FILES_QUEUE_CHANNEL)
             .get();
     }
 
     @Bean IntegrationFlow outboundFileUploaderEndpoint() {
         return IntegrationFlows
-            .from("filesQueueChannel")
-            .handle(gDriveUploader())
+            .from(FILES_QUEUE_CHANNEL)
+            .handle(uploader())
             .get();
     }
 
-    @NotNull private GenericHandler<File> gDriveUploader() {
+    @NotNull private GenericHandler<File> uploader() {
         return (payload, headers) -> {
             log.info("PAYLOAD: " + payload);
-            log.info("HEADERS: " + headers);
+            // log.info("HEADERS: " + headers);
             return null;
         };
     }
 
     @Bean QueueChannelSpec filesQueueChannel() {
-        return MessageChannels.queue();
+        return MessageChannels.queue(BATCH_SIZE);
     }
 
     @Bean(name = PollerMetadata.DEFAULT_POLLER)
     PollerMetadata defaultPoller() {
         return fixedDelay(3, TimeUnit.SECONDS)
-            .maxMessagesPerPoll(MAX_MESSAGES_PER_POLL)
+            .maxMessagesPerPoll(BATCH_SIZE)
             .get();
     }
 }
