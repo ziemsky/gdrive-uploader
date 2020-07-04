87f202bfc4f28043fb40dd8cd93cbba1fcda41d5
Rafał Kociński <rk@ziemsky.com>
2020-07-04 16:19:03 +0100

    fix #19 GitRepo - allTagsNames

--- gitversionreleaseplugin/src/main/kotlin/com/ziemsky/gradle/gitversionreleaseplugin/GitRepo.kt
+++ gitversionreleaseplugin/src/main/kotlin/com/ziemsky/gradle/gitversionreleaseplugin/GitRepo.kt
@@ -1,13 +1,40 @@
 package com.ziemsky.gradle.gitversionreleaseplugin
 
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.lib.Constants.R_TAGS
 import java.io.File
+import java.nio.file.Path
 
-class GitRepo private constructor(dir: File) {
+class GitRepo private constructor(val gitRepoDir: Path) {
 
     companion object Factory {
-        fun of(gitRepoDir: File): GitRepo {
+
+        /**
+         * @param gitRepoDir Directory that contains '.git'.
+         */
+        fun at(gitRepoDir: Path): GitRepo {
             return GitRepo(gitRepoDir)
+        }
+
+        /**
+         * @param gitRepoDir Directory that contains '.git'.
+         */
+        fun at(gitRepoDir: File): GitRepo {
+            return GitRepo(gitRepoDir.toPath())
         }
     }
 
+    override fun toString(): String {
+        return "GitRepo(dir=$gitRepoDir)"
+    }
+
+    fun allTagsNames(): Set<String> {
+        return repository { git: Git -> git.repository.refDatabase.getRefsByPrefix(R_TAGS) }
+                .map { it.name.removePrefix("refs/tags/") }
+                .toSet()
+    }
+
+    private fun <T> repository(function: (Git) -> T): T {
+        return Git.open(gitRepoDir.toFile()).use(function)
+    }
 }
---
+++ gitversionreleaseplugin/src/test/kotlin/com/ziemsky/gradle/gitversionreleaseplugin/GitRepoSpec.kt
@@ -0,0 +1,95 @@
+package com.ziemsky.gradle.gitversionreleaseplugin
+
+import io.kotest.core.listeners.TestListener
+import io.kotest.core.spec.IsolationMode
+import io.kotest.core.spec.Spec
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.apache.commons.io.FileUtils.deleteDirectory
+import java.nio.file.Files.createTempDirectory
+import java.nio.file.Files.exists
+import java.nio.file.Path
+import java.nio.file.Paths
+
+class GitRepoSpec : BehaviorSpec() {
+
+    override fun isolationMode(): IsolationMode? = IsolationMode.InstancePerLeaf
+
+    init {
+        listener(specTestDirListener)
+
+        Given("""Local Git repo with several commits tagged,
+            | mixing lightweight tags with annotated ones
+            | where tags are applied on various branches
+        """.trimMargin()) {
+
+            val gitRepo: GitRepo = repo("gitRepoWithMixedVersionAndOtherTags")
+
+            println("gitRepo: $gitRepo")
+
+            When("asked for list of all tags") {
+
+                val allTags = gitRepo.allTagsNames()
+
+                Then("returns all tags") {
+
+                    allTags shouldBe setOf(
+                            "v0.0.0",
+                            "v0.1.0",
+                            "v10.1.0",
+                            "version@0.1.0",
+                            "version@0.2.0"
+                    )
+                }
+            }
+        }
+    }
+
+    private fun repo(testRepoDirectoryName: String): GitRepo {
+
+        val sourceRepoDir: Path = testDataFile(testRepoDirectoryName)
+
+        val targetRepoDir: Path = Paths.get(
+                specTestDirListener.specTestDir.toAbsolutePath().toString(),
+                testRepoDirectoryName,
+                ".git"
+        )
+
+        sourceRepoDir.toFile().copyRecursively(targetRepoDir.toFile())
+
+        return GitRepo.at(targetRepoDir)
+    }
+
+    private fun testDataFile(testFileName: String): Path {
+
+        val testFileResourceName = "test-data/${GitRepoSpec::class.simpleName}/$testFileName"
+
+        return GitRepoSpec::class.java.classLoader.getResource(testFileResourceName)
+                .let { if (it == null) throw IllegalStateException("Test file $testFileName not found."); it }
+                .let { it.toURI() }
+                .let { Paths.get(it) }
+    }
+
+    object specTestDirListener : TestListener {
+
+        lateinit var specTestDir: Path
+
+        override suspend fun beforeSpec(spec: Spec) {
+            specTestDir = withContext(Dispatchers.IO) {
+                createTempDirectory(GitRepoSpec::class.qualifiedName + ".")
+            }
+        }
+
+        override suspend fun afterSpec(spec: Spec) {
+            specTestDir.let { dir: Path ->
+                withContext(Dispatchers.IO) {
+                    if (exists(dir)) {
+                        deleteDirectory(dir.toFile())
+                    }
+                }
+            }
+        }
+    }
+}
